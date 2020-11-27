cdb492cc649f6e7f25cd406cf3a1fdf61499030b
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-27 19:05:18 +0000

    coded garage_management.sql and minor changes to three-class diagram

---
+++ source_code/db/garage_management.sql
@@ -0,0 +1,26 @@
+DROP TABLE repairs;
+DROP TABLE mechanics;
+DROP TABLE cars;
+
+CREATE TABLE mechanics (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    mot_qualified BOOLEAN
+)
+
+CREATE TABLE cars (
+    id SERIAL PRIMARY KEY,
+    registration_number VARCHAR(255),
+    make VARCHAR(255),
+    model VARCHAR(255),
+    mot_renewal_date DATE 
+)
+
+CREATE TABLE repairs (
+    id SERIAL PRIMARY KEY,
+    mechanic_id INT REFERENCES mechanics(id) ON DELETE CASCADE,
+    car_id INT REFERENCES cars(id) ON DELETE CASCADE,
+    repair_date DATE
+    details VARCHAR(255)
+)
