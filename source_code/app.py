251719950cdaf58d1952356ef29c39abd0348b19
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-29 10:58:22 +0000

    coded app.py for mechanics

---
+++ source_code/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+from controllers.mechanic_controller import mechanics_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(mechanics_blueprint)
+
+@app.route('/')
+def homw():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
