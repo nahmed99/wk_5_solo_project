3e1ff78446655e9a94e608973607a7c902a408f4
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-29 10:19:16 +0000

    coded delete functions in car_repository.py

--- source_code/repositories/car_repository.py
+++ source_code/repositories/car_repository.py
@@ -25,3 +25,28 @@ def select_all():
         cars.append(car)
 
     return cars
+
+
+def select(id):
+    car = None
+
+    sql = "SELECT * FROM cars WHERE id = %s"
+    values = [id]
+
+    result = run_sql(sql, values)[0]  # although only 1 row should be returned, making sure with the [0]..?
+
+    if result is not None:
+        car = Car(result['registration_number'], result['make'], result['model'], result['mot_renewal_date'], result['id'])
+
+    return car
+
+
+def delete(id):
+    sql = "DELETE FROM cars WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def delete_all():
+    sql = "DELETE FROM cars"
+    run_sql(sql)
