6d8d426d7bedd0111b51ee817e165e162e611be8
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-29 10:06:54 +0000

    coded save function in car_repository

---
+++ source_code/repositories/car_repository.py
@@ -0,0 +1,15 @@
+from db.run_sql import run_sql
+from models.car import Car
+from models.mechanic import Mechanic
+
+
+def save(car):
+    sql = "INSERT INTO cars(registration_number, make, model, mot_renewal_date) VALUES (%s, %s, %s, %s) RETURNING id"
+    values = [car.registration_number, car.make, car.model, car.mot_renewal_date]
+
+    results = run_sql(sql, values)
+
+    car.id = results[0]['id']
+
+    return car
+
