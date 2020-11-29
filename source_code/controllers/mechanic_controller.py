0870100f4ac23a934f97c4cf2d9b33cd4e473f75
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-29 10:36:49 +0000

    coded mechanic_controller.py. Just beginning to code base.html

---
+++ source_code/controllers/mechanic_controller.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from flask import Blueprint
+from models.mechanic import Mechanic
+import repositories.mechanic_repository as mechanic_repository
+
+mechanics_blueprint = Blueprint("mechanic", __name__)
+
+@mechanics_blueprint.route("/mechanic")
+def mechanic():
+    mechanics = mechanic_repository.select_all
+
+    return render_template("mechanics/index.html", all_mechanics = mechanics)
+
+
+@mechanics_blueprint.route("/mechanics/<id>")
+def show(id):
+    mechanic = mechanic_repository.select(id)
+    mechanic_cars_repaired = mechanic_repository.cars_repaired(mechanic)
+
+    return render_template("mechanics/show.html", mechanic=mechanic, cars_repaired=mechanic_cars_repaired)
