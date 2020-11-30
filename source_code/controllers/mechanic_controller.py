a69c1f0af6b0f8a6d18cfeeabfcd447bc3c361a7
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-30 13:01:19 +0000

    Add new mechanic coded

--- source_code/controllers/mechanic_controller.py
+++ source_code/controllers/mechanic_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.mechanic import Mechanic
 import repositories.mechanic_repository as mechanic_repository
@@ -18,3 +18,30 @@ def show(id):
     mechanic_cars_repaired = mechanic_repository.cars_repaired(mechanic)
 
     return render_template("mechanics/show.html", mechanic=mechanic, cars_repaired=mechanic_cars_repaired)
+
+
+
+# NEW
+# GET '/mechanics/new'
+@mechanics_blueprint.route("/mechanics/new", methods = ['GET'])
+def new_mechanic():
+    # Render html template for new mechanic 
+    return render_template('mechanics/new.html')
+
+
+# CREATE
+# POST '/mechanics'
+@mechanics_blueprint.route("/mechanics", methods=['POST'])
+def create_mechanic():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    mot_qualified = request.form['mot_qualified']
+    
+    mechanic = Mechanic(first_name, last_name, mot_qualified)
+
+    # send request to save mechanic to database
+    mechanic_repository.save(mechanic)
+
+    # return to mechanics webpage
+    return redirect('/mechanics')
+
