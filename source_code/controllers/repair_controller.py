4149fede9bfee344501c49e32f203bca5b787cee
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-30 11:24:47 +0000

    Delete a single repair is working

--- source_code/controllers/repair_controller.py
+++ source_code/controllers/repair_controller.py
@@ -19,3 +19,9 @@ def show(mechanic_id, car_id):
     repairs = repair_repository.get_rapair_details(mechanic_id, car_id)
     return render_template("/repairs/show.html", car_repairs = repairs)
 
+
+@repairs_blueprint.route("/repairs/<id>/delete", methods=['POST'])
+def delete_repair(id):
+    repair_repository.delete(id)
+    return redirect("/repairs")
+
