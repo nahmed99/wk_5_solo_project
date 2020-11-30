d4e45833ef5b96de091df96b2a23b6498199048e
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-30 16:52:36 +0000

    coded car_controller.py to create new car

--- source_code/controllers/car_controller.py
+++ source_code/controllers/car_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.car import Car
 import repositories.car_repository as car_repository
@@ -18,3 +18,31 @@ def show(id):
     car_worked_on_by = car_repository.worked_on_by(car)
 
     return render_template("cars/show.html", car=car, mechanics=car_worked_on_by)
+
+
+# NEW
+# GET '/cars/new'
+@cars_blueprint.route("/cars/new", methods = ['GET'])
+def new_car():
+    # Render html template for new car
+    return render_template('cars/new.html')
+
+
+# CREATE
+# POST '/cars'
+@cars_blueprint.route("/cars", methods=['POST'])
+def create_car():
+
+    registration_number = request.form['registration_number']
+    make = request.form['make']
+    model = request.form['model']
+    mot_renewal_date = request.form['mot_renewal_date']
+
+    car = Car(registration_number, make, model, mot_renewal_date)
+
+    # send request to save mechanic to database
+    car_repository.save(car)
+
+    # return to mechanics webpage
+    return redirect('/cars')
+    
